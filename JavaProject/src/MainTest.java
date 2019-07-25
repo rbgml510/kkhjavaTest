289c2ee49bac29fa21562bd64a3001c39ac70986
asus <asus@192.168.0.9>
2019-07-25 12:38:56 +0900

    commit test

---
+++ JavaProject/src/MainTest.java
@@ -0,0 +1,33 @@
+
+public class MainTest {
+
+	private String id;
+	private String name;
+	private String addr;
+	private String pass;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+}
